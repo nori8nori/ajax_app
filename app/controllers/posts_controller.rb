30028d5f6e92ea643b5d5b0a8e80aff77c4b0990
nori8nori <69037486+nori8nori@users.noreply.github.com>
2020-08-14 16:05:06 +0900

    heroku導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,16 +7,18 @@ class PostsController < ApplicationController
   # end
 
   def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
+  def create
     # Post.create(content: params[:content])
     # redirect_to action: :index
-    post = Post.create(content: parms[:content],checkd:false)
+    post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
   end
 
   def checked
-
-    # binding.pry
-
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
